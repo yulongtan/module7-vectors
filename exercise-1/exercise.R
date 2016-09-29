fae7eb98a2158b7d5282d148f140967152a441fa
mkfreeman <michael.k.freeman@gmail.com>
2016-09-28 22:41:47 -0700

    Fixed typo

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,28 +1,27 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
-x <- 10:20
+
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
-y <- seq(21, 30)
+
 
 # Create a vector `z` by combining the vectors `x` and `y`
-x <- c(x,y)
+
 
 # Create a variable `w` that contains the 11th element in your vector `z`
-w <- z[11]
+
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
-q <- z[2:5]
+
 
 # Create a variable `len` that is equal to the length of your vector `q`
-len <- length(q)
+
 
 ### Bonus ###
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-even <- seq(2, 100, 2) # start at first even number!
+
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-test <- all(even %% 2 == 0)
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,40 +1,24 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 colors (representing marbles).
-marbles <- c('red', 'blue', 'green', 'yellow', 'magenta', 'cyan')
+
 
 # Use the `sample()` function to randomly select a single marble
-my.marble <- sample(marbles, 1)
+
 
 # Define a function called MarbleGame. This function should:
 # - Takes in a single argument `guess` that is a guess at what color marble will be drawn
 # - Randomly samples a marble from the vector
 # - Returns whether or not the person guessed accurately (preferrably a character string phrase)
 
-MarbleGame <- function(guess) {
-  marble <- sample(marbles, 1)
-  if(marble == guess) {
-    return("You won!")
-  }else {
-    return("You lost :(")
-  }
-}
+
 
 # Play the marble game by guessing a color!
-MarbleGame('blue')
+
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 # Hint, you'll need to use a `while` control structure, which is not covered in the module
-have.not.won <- TRUE
-tries <- 0
-while(have.not.won) {
-  tries <- tries + 1
-  result <- MarbleGame(marble.bag, 'blue') # try to guess 'blue'
-  if(result == "You won!") {
-    have.not.won <- FALSE
-  }
-}
-print(tries)
+
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
